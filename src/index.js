77e8ab852329fdb065c6ca0b68ea24f08dacaa5d
Vagyasri <vagyasri2011@gmail.com>
2021-10-28 12:30:14 +0200

    Imply Adjustment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const inputs = document.querySelectorAll('.controls input');
+
+const handleUpdate = (e) => {
+  const tar = e.target;
+  const suffix = tar.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${tar.name}`, tar.value + suffix);
+};
+
+inputs.forEach((input) => input.addEventListener('change', handleUpdate));
+// inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
